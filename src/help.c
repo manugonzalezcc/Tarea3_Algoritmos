7d7979bf988d6d1ba3ce38fb1ec9ac36459e0c3d
Diego0119 <diegosanhuezaaguilar@gmail.com>
2025-06-02 09:58:20 -0300

    mejorando el main y aceptando argumentos

--- src/help.c
+++ src/help.c
@@ -2,10 +2,11 @@
 
 void print_help(const char *prog_name)
 {
-    printf("Uso: %s [opciones]\n", prog_name);
+    printf("Uso: %s -f <archivo> [-kmp | -bm | -algoritmo3]\n", prog_name);
     printf("Opciones:\n");
-    printf("  -f <archivo>   Archivo de texto a procesar (requerido)\n");
-    printf("  -v             Modo verbose (mostrar detalles adicionales)\n");
-    printf("  -h, --help     Mostrar esta ayuda y salir\n");
+    printf("  -f <archivo>       Archivo de texto a procesar (requerido)\n");
+    printf("  -kmp               Explica el algoritmo Knuth-Morris-Pratt\n");
+    printf("  -bm                Explica el algoritmo Boyer-Moore\n");
+    printf("  -algoritmo3        Explica el tercer algoritmo personalizado\n");
+    printf("  -h, --help         Mostrar esta ayuda y salir\n");
 }
-
--- src/main.c
+++ src/main.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <getopt.h>
 #include "tokenizer.h"
 #include "search.h"
 #include "help.h"
-#include "boyer_moore.h" // <-- Agrega esta línea
+#include "boyer_moore.h"
 
 int main(int argc, char *argv[])
 {
     int opt;
     char *archivo = NULL;
-    int verbose = 0;
 
-    while ((opt = getopt(argc, argv, "f:vh")) != -1)
+    int usar_kmp = 0;
+    int usar_bm = 0;
+    int usar_algoritmo3 = 0;
+
+    static struct option long_options[] = {
+        {"file", required_argument, 0, 'f'}, // -f o --file
+        {"verbose", no_argument, 0, 'v'},    // -v o --verbose
+        {"help", no_argument, 0, 'h'},       // -h o --help
+        {"kmp", no_argument, 0, 1},          // --kmp
+        {"bm", no_argument, 0, 2},           // --bm
+        {"algoritmo3", no_argument, 0, 3},   // --algoritmo3
+        {0, 0, 0, 0}};
+
+    int long_index = 0;
+
+    while ((opt = getopt_long(argc, argv, "f:vh", long_options, &long_index)) != -1)
     {
         switch (opt)
         {
@@ -21,16 +37,20 @@ int main(int argc, char *argv[])
         case 'h':
             print_help(argv[0]);
             return 0;
-        case 'v':
-            verbose = 1;
+        case 1:
+            usar_kmp = 1;
+            break;
+        case 2:
+            usar_bm = 1;
+            break;
+        case 3:
+            usar_algoritmo3 = 1;
             break;
         default:
-            fprintf(stderr, "Uso: %s -f archivo [-v]\n", argv[0]);
+            fprintf(stderr, "Uso: %s -f <archivo> [-kmp | -bm | -algoritmo3]\n", argv[0]);
             return 1;
         }
     }
-
-    printf("DEBUG: archivo = %s\n", archivo); // <-- agrega esto
 
     if (archivo == NULL)
     {
@@ -38,17 +58,27 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    printf("Archivo: %s\n", archivo);
-    if (verbose)
+    if (!usar_kmp && !usar_bm && !usar_algoritmo3)
     {
-        printf("Modo verbose activado\n");
+        fprintf(stderr, "Debes especificar uno de: --kmp, --bm, --algoritmo3\n");
+        return 1;
     }
 
-    // Prueba de Boyer-Moore
-    const char *texto = "este es un texto de prueba, texto de ejemplo";
-    const char *patron = "texto";
-    int ocurrencias = boyer_moore_bad_char(texto, patron);
-    printf("Boyer-Moore: El patrón '%s' aparece %d veces en el texto.\n", patron, ocurrencias);
+    if (usar_kmp)
+    {
+        printf("Ejecutando búsqueda con KMP...\n");
+    }
+    else if (usar_bm)
+    {
+        const char *texto = "este es un texto de prueba, texto de ejemplo";
+        const char *patron = "texto";
+        int ocurrencias = boyer_moore_bad_char(texto, patron);
+        printf("Boyer-Moore: El patrón '%s' aparece %d veces en el texto.\n", patron, ocurrencias);
+    }
+    else if (usar_algoritmo3)
+    {
+        printf("Ejecutando búsqueda con algoritmo3...\n");
+    }
 
     return 0;
 }
